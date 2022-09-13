637a34b6a843b06270e623cfca1a2ff9b56fe4bf
AguJP <agujpa@gmail.com>
2022-09-12 21:58:11 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
 
 /* Function main to returns void */
 int main(void)
-/*Function main prints texts to std output, returns void */
+/* Description: Function main prints texts to std output, returns void */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	/* Program to return 0 */
