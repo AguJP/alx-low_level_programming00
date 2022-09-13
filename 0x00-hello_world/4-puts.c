712d6efe6d19023740091c068c3777415f86fdb0
AguJP <agujpa@gmail.com>
2022-09-12 21:15:54 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 /* Program to print characters to std output */
 #include <stdio.h>
 /* Include std input and output header files*/
-int main(void) /* Main function to return void */
+/* Main to return void */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	/* Program to return 0 */
