71acbcd829262428b83119a7be6bf6a8ec6237ae
AguJP <agujpa@gmail.com>
2022-09-13 11:51:18 -0700

    Print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+/**
+ * Program to print all possible combination of single
+ * digit numbers 0 to 10 using putchar
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Description: Program to print all possible combination of
+ * single digit numbers 0 to 10 using putchar
+ *
+ * Return: Program to return 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+
+/* Program to return 0 (Success) */
+	return (0);
+}
