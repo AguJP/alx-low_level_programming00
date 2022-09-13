e045e9ae7b1bf16768d1a0a695dbb92e96673802
AguJP <agujpa@gmail.com>
2022-09-12 23:35:31 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/* Program to print text using printf function */
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Program to print text using printf'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf(%s, "with proper grammar, but the outcome is a piece of art, ");
+	/* Program returns 0 (Success) */
+	return (0);
+}
