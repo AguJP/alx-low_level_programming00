406d47a7ba02283e6a6bc854df69c9f5cd7e4d32
AguJP <agujpa@gmail.com>
2022-09-13 08:48:39 -0700

    Print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+/* Program to print selected alphabets in lowercase */
+
+#include <stdio.h>
+
+/**
+ * Main - Program entry point
+ *
+ * Description: Program to print lowercase alphabets except q and e
+ *
+ * Return: Program to return 0 (Success)
+ */
+
+int main(void)
+{
+	char abichiidi;
+
+	for (abichiidi = 'a';abichiidi <= 'z';abichiidi++)
+	{
+		if (abichiidi != ('e' | 'q')
+			putchar(abichiidi);
+		    }
+
+		    putchar('\n');
+/* Program to return 0 (Success) */
+		    return (0);
+}
