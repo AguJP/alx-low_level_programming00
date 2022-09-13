83677151298d25fbe267de4d72782ba1227ef4fc
AguJP <agujpa@gmail.com>
2022-09-13 10:06:19 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+/* Program to print single digit numbers 0 to 10 */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Description: Program to print single digit numbers 0 to 10
+ *
+ * Return: Program to return 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+
+/* Program to return 0 (Success) */
+	return (0);
+}
