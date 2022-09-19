1acc3fb2dd68c826d405fb5215c5c5418b41c636
AguJP <agujpa@gmail.com>
2022-09-19 03:55:00 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,21 +12,21 @@ void jack_bauer(void)
 	int hr = 0;
 	int mn = 0;
 
-	for (hr = 0; hr <= 23; hr++)
+	for (hr = "0"; hr <= "23"; hr++)
 	{
-		for (mn = 0; mn <= 59; mn++)
+		for (mn = "0"; mn <= "59"; mn++)
 		{
 			if (hr <= 9)
 			{
 				_putchar('0');
 			}
-			_putchar(int hr + '0');
+			_putchar(hr + '0');
 			_putchar(':');
 			if (mn <= 9)
 			{
 				_putchar('0');
 			}
-			_putchar(int mn + '0');
+			_putchar(mn + '0');
 			_putchar('\n');
 		}
 	}
