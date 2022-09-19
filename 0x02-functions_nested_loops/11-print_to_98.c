b5e0d3ae7e8aa3719f5dcafe43a2a762b1b574bf
AguJP <agujpa@gmail.com>
2022-09-19 15:48:38 -0700

    Print 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,17 +14,13 @@ void print_to_98(int n)
 	if (n <= 98)
 		while (n <= 98)
 		{
-			_putchar(n / 10 + '0');
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, " n);
 			n++;
 		}
 	else
 		while (n >= 98)
 		{
-			_putchar(n / 10 + '0');
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, " n);
 			n--;
 		}
 
