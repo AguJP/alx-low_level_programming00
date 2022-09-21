65752678cf1d9851763f0f3a46e38e038e79274f
AguJP <agujpa@gmail.com>
2022-09-21 14:58:54 -0700

    Print most

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,10 +10,10 @@ void print_most_numbers(void)
 {
 	char i = '0';
 
-	while (i <= 9)
+	while (i <= '9')
 	{
-		if (i != 2 || i != 4)
-			_putchar(i + '0');
+		if (i != '2' || i != '4')
+			_putchar(i);
 		else
 
 		i++;
