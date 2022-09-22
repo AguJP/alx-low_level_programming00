3c4e8e768e3b570369ee28e63f3b3b4099011b7b
AguJP <agujpa@gmail.com>
2022-09-22 03:27:46 -0700

    Print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -23,10 +23,10 @@ void print_triangle(int size)
 			{
 				k--;
 				m++;
-				for (l = 1; l <= k; l++)
-					_putchar('#');
-				for (n = 1; n <= m; n++)
+				for (l = 0; l <= k; l++)
 					_putchar('.');
+				for (n = 0; n <= m; n++)
+					_putchar('#');
 			}
 
 			_putchar('\n');
