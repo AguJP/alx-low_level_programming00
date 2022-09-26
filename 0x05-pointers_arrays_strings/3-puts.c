cb6d9b719177c86a37168119e26eb923b4bb4000
AguJP <agujpa@gmail.com>
2022-09-26 10:09:12 -0700

    Puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - function to print a string followed by a newline to stdout
+ * @str: string variable to be printed
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+
+}
